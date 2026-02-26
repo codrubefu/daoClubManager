d6090e2943b3c0a47cfd17068e0bf91a29d35597
Codrut Befu <codrut_befu@yahoo.com>
2026-02-26 16:15:13 +0200

    Add RBAC user CRUD and parent-student linkage APIs

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'email' => ['sometimes', 'required', 'email'],
+            'password' => ['sometimes', 'required', 'string', 'min:8'],
+            'role' => ['sometimes', 'required', 'in:admin,coach,student,parent'],
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\ParentStudentController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\ResolveTenant;
 use Illuminate\Support\Facades\Route;
@@ -13,4 +14,12 @@ Route::post('/auth/refresh', [AuthController::class, 'refresh'])->middleware(Res
 Route::middleware([ResolveTenant::class, 'auth:sanctum'])->group(function (): void {
     Route::get('/users', [UserController::class, 'index']);
     Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::patch('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+    Route::get('/parents/me/children', [ParentStudentController::class, 'myChildren']);
+    Route::get('/parents/{parent}/children', [ParentStudentController::class, 'index']);
+    Route::post('/parents/{parent}/children/{student}', [ParentStudentController::class, 'link']);
+    Route::delete('/parents/{parent}/children/{student}', [ParentStudentController::class, 'unlink']);
 });
